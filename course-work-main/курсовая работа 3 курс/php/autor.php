d17b045e86cda40447d96e8ad5b51a11e762b88a
kyvshinka2 <115142098+kyvshinka2@users.noreply.github.com>
2023-04-18 11:26:38 +0500

    Add files via upload

---
+++ course-work-main/курсовая работа 3 курс/php/autor.php
@@ -0,0 +1,36 @@
+<?php
+$login = $_POST['login'];
+$password = $_POST['password'];
+
+if (mb_strlen($login) < 5 || mb_strlen($login) > 40) {
+    echo "Недопустимая длина логина (от 5 до 40 символов)";
+    // после этой строчки код не будет работать дальше
+    exit(); 
+} else if (mb_strlen($password) < 2 || mb_strlen($password) > 6) {
+    echo "Недопустимая длина пароля (от 2 до 6 символов)";
+    exit();
+}
+
+$mysql = new mysqli('localhost', 'root', '', 'English_School');
+$mysql->set_charset('utf8');
+if($mysql->connect_error){
+    die("Ошибка: " . $mysql->connect_error);
+}
+/*
+$result2 = "SELECT * FROM `Student` WHERE `Log_In` = '$login' AND `Password` = '$password'";
+$user = $mysqli->query($result2);*/
+$result = $mysqli->query("SELECT * FROM `Student` WHERE `Log_In` = '$login' AND `Password` = '$password'");
+if ($result->num_rows > 0) {
+    echo "Такого пользователя нет";
+    exit(); 
+}
+/*if (count($user) == 0) {
+    echo "Такого пользователя нет";
+    exit();
+}*/
+
+setcookie('User', $user['name'], time() + 3600, "/");
+
+header('Location: ../page/root.html');
+$mysql->close();
+?>
